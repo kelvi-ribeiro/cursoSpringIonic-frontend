4dc32c9379043aa2a181bbf9243177692ac92b50
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-03-24 04:09:04 -0300

    Aula 106. Implementando Lazy Loading em HomePage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
 
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -12,7 +12,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+
   ],
   imports: [
     BrowserModule,
@@ -21,7 +21,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+
   ],
   providers: [
     StatusBar,
